49ffd4297abc03aa6a827baa84064e32eea631fd
Timothy Purnawan <timothy.purnawan@binus.edu>
2024-06-18 15:19:17 +0700

    add styling to payment page

--- client/javascript/payment.js
+++ client/javascript/payment.js
@@ -27,47 +27,55 @@ window.onload = async () => {
         window.location.href = 'index.html'
     }
 
-    const court = await getCourtByCourtId(courtId)
+    // const court = await getCourtByCourtId(courtId)
 
-    schedules = schedules.split(',')
-    let res = ''
-    const scheduleTypes = await getScheduleTypes()
+    // schedules = schedules.split(',')
+    // let res = ''
+    // const scheduleTypes = await getScheduleTypes()
 
-    schedules.forEach(schedule => {
-        res += `${scheduleTypes[schedule].startTime}-${scheduleTypes[schedule].endTime},`
-    });
+    // schedules.forEach(schedule => {
+    //     res += `${scheduleTypes[schedule].startTime}-${scheduleTypes[schedule].endTime},`
+    // });
 
-    const name = document.createElement('p')
-    name.innerHTML = `Name: ${court[0].courtName}`
-    const date2 = document.createElement('p')
-    date2.innerHTML = date
-    const schedule = document.createElement('p')
-    schedule.innerHTML = res
-    const button = document.createElement('button')
-    button.textContent = 'Pay'
-    button.addEventListener('click', async () => {
-        document.getElementById('background').style.display = 'block';
-        document.getElementById('paymentPopUp').style.display = 'block';
+    // const name = document.createElement('p')
+    // name.innerHTML = `Name: ${court[0].courtName}`
+    // const date2 = document.createElement('p')
+    // date2.innerHTML = date
+    // const schedule = document.createElement('p')
+    // schedule.innerHTML = res
+    // const button = document.createElement('button')
+    // button.textContent = 'Pay'
+    // button.addEventListener('click', async () => {
+    //     document.getElementById('background').style.display = 'block';
+    //     document.getElementById('paymentPopUp').style.display = 'block';
 
-        const res = await insertSchedule(courtId, date, schedules, userId)
-        console.log(res)
-        setTimeout(() => {
-            window.location.href = 'booking.html'
-        }, 3000)
-    })
+    //     const res = await insertSchedule(courtId, date, schedules, userId)
+    //     console.log(res)
+    //     setTimeout(() => {
+    //         window.location.href = 'booking.html'
+    //     }, 3000)
+    // })
 
-    document.getElementById('paymentDiv').appendChild(name)
-    document.getElementById('paymentDiv').appendChild(date2)
-    document.getElementById('paymentDiv').appendChild(schedule)
-    document.getElementById('paymentDiv').appendChild(button)
+    // document.getElementById('paymentDiv').appendChild(name)
+    // document.getElementById('paymentDiv').appendChild(date2)
+    // document.getElementById('paymentDiv').appendChild(schedule)
+    // document.getElementById('paymentDiv').appendChild(button)
 
 }
 
-// document.getElementById('payButton').addEventListener('click', () => {
+document.getElementById('confirmButton').addEventListener('click', async () => {
+    document.getElementById('background').style.display = 'block';
+    document.getElementById('paymentPopUp').style.display = 'flex';
 
-// })
+    const userId = localStorage.getItem('user')
+    const courtId = getParam('court-id')
+    const date = getParam('date')
+    let schedules = getParam('schedule')
+    schedules = schedules.split(',')
 
-// document.getElementById('background').addEventListener('click', () => {
-//     document.getElementById('background').style.display = 'none';
-//     document.getElementById('paymentPopUp').style.display = 'none';
-// })
+    const res = await insertSchedule(courtId, date, schedules, userId)
+    console.log(res)
+    setTimeout(() => {
+        window.location.href = 'booking.html'
+    }, 3000)
+})
